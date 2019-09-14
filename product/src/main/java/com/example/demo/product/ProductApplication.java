afffcabe8f96e39e63e933974b943dc5df279602
venkateshpamulapati <venkateshpamulapati@github.com>
2019-09-14 16:43:31 +0530

    addded eureka server

--- product/src/main/java/com/example/demo/product/ProductApplication.java
+++ product/src/main/java/com/example/demo/product/ProductApplication.java
@@ -3,8 +3,9 @@ package com.example.demo.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@EnableDiscoveryClient
 public class ProductApplication {
 
 	public static void main(String[] args) {
